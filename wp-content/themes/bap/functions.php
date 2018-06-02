dbdb0faa9f59dd0354cd79a452ef47005aa1bb82
Pierre Vanlierde <liverdan@ntymail.com>
2018-06-02 22:03:33 +0200

    Ajout menu Second

--- wp-content/themes/bap/functions.php
+++ wp-content/themes/bap/functions.php
@@ -45,6 +45,7 @@ if ( ! function_exists( 'bap_setup' ) ) :
 		// This theme uses wp_nav_menu() in one location.
 		register_nav_menus( array(
 			'menu-1' => esc_html__( 'Primary', 'bap' ),
+			'menu-2' => esc_html__( 'Second', 'bap' ),
 		) );
 
 		/*
