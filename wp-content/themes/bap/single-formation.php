dee30df630cad8de8048eb656f67840dda22f383
Pierre Vanlierde <liverdan@ntymail.com>
2018-07-06 10:57:52 +0200

    Préparation incorporation Formulaire de test

--- wp-content/themes/bap/single-formation.php
+++ wp-content/themes/bap/single-formation.php
@@ -12,7 +12,7 @@ get_header();
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
-<h1>Page single.php</h1>
+<h1>Page single-formation.php</h1>
 		<?php
 		while ( have_posts() ) :
 			the_post();
