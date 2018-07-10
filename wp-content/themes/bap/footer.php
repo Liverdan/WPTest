6c328c83ba45d7c1115c98a39b1d62b91ea2fac7
Pierre Vanlierde <liverdan@ntymail.com>
2018-07-10 14:52:11 +0200

    Changement siganture dans le footer

--- wp-content/themes/bap/footer.php
+++ wp-content/themes/bap/footer.php
@@ -15,13 +15,13 @@
 
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'bap' ) ); ?>">
+			<a href="<?php echo esc_url( __( 'https://www.linkedin.com/in/pierre-vanlierde-b2a637a4', 'bap' ) ); ?>">
 				<?php
 				/* translators: %s: CMS name, i.e. WordPress. */
-				/* printf( esc_html__( 'Proudly powered by %s', 'bap' ), 'WordPress' );*/
+				printf( esc_html__( 'Powered by %s', 'bap' ), 'P.Vanlierde' );
 				?>
 			</a>
-			<span class="sep"> | </span>
+			<span class="sep">  </span>
 				<?php
 				/* translators: 1: Theme name, 2: Theme author. */
 				/*printf( esc_html__( 'Theme: %1$s by %2$s.', 'bap' ), 'bap', '<a href="http://underscores.me/">Underscores.me</a>' );*/
